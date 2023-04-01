9f5a23c552ba18d3623f65b8e8725d0c49fa8e2e
hitesh <hiteshpratap82@gmail.com>
2023-04-02 00:42:48 +0530

    message framework

--- account/static/js/script.js
+++ account/static/js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    console.log("hi")
+    console.log("hisff")
     $("#id_date_of_birth").attr("type", "date");
     // console.log($("#id_date_of_birth").type)
 })
 
 // document.getElementById("id_date_of_birth").type = 'date'
+
+setTimeout(function(){
+    $(".messages").fadeOut("slow")
+},4000)
--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 from account.forms import LoginForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -80,6 +81,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request,'Profile updated \
+                                        succesfully')
+        else:
+            messages.error(request,'Error updating your profile')
+
     else:
         user_form = UserEditForm(instance=request.user) 
         profile_form = ProfileEditForm(instance = request.user.profile)
