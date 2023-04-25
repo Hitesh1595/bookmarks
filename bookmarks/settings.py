0feafaef1573546d0521c44b9c5535837c2600e7
hitesh <hiteshpratap82@gmail.com>
2023-04-25 23:36:47 +0530

    signals for like filed and debug app install

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -29,6 +29,10 @@ THUMBNAIL_DEBUG = True
 
 ALLOWED_HOSTS = ['mysite.com','localhost','127.0.0.1']
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 
 # Application definition
 
@@ -48,11 +52,14 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'easy_thumbnails',
+    # django toolbar checks DEBUG settins True
+    'debug_toolbar',
 
   
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
